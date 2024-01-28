33c810b1aa2d1727e02a3733998d0203b6aa33a5
yoo16 <yohei.yoshikawa@gmail.com>
2024-01-28 14:29:02 +0900

    update

--- client/app/providers/UserProvider.tsx
+++ client/app/providers/UserProvider.tsx
@@ -2,28 +2,23 @@
 
 import React, { useEffect, useState } from "react";
 import UserContext, { UserContextType } from "../context/UserContext";
-import { User, initialUser } from "../models/User";
-import { getAccessToken, getUser } from "../services/UserService";
+import { initialUser } from "../models/User";
+import { getUser } from "../services/UserService";
 import Cookies from "js-cookie";
-// import { SessionProvider } from "next-auth/react"
 
 export default function UserProvider({
     children,
 }: {
     children: React.ReactNode,
 }): React.ReactNode {
-    console.log("--- UserProvider ---")
-
     const [user, setUser] = useState(initialUser)
 
     useEffect(() => {
         (async () => {
-            const token = Cookies.get('access_token');
-            if (token) {
-                const user = await getUser(token);
-                console.log("AuthProvider:", user)
-                setUser(user);
-            }
+            const token = Cookies.get('access_token') || "";
+            const user = await getUser(token);
+            console.log("AuthProvider:", user)
+            setUser(user);
         })();
     }, [])
 
@@ -38,8 +33,5 @@ export default function UserProvider({
         <UserContext.Provider value={value}>
             {children}
         </UserContext.Provider>
-        // <SessionProvider session={session}>
-        //     {children}
-        // </SessionProvider>
     )
 }

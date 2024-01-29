38c46ef7fe525c38fbf23609040bf102c84625ef
yoo16 <yohei.yoshikawa@gmail.com>
2024-01-29 19:17:06 +0900

    update

--- client/app/providers/NextAuthProvider.tsx
+++ client/app/providers/NextAuthProvider.tsx
@@ -1,7 +1,6 @@
 "use client";
 
-import { SessionProvider, useSession } from "next-auth/react";
-import { User } from "../models/User";
+import { SessionProvider } from "next-auth/react";
 
 type Props = {
     children?: React.ReactNode;
